e1a687d87eb47e86bc583051dee4f848c04b8129
Michael Newbold <newboldm3@gmail.com>
2023-12-06 17:50:17 -0800

    further day 1 progress

--- day1/day1.js
+++ day1/day1.js
@@ -14,4 +14,13 @@ const dataSplit = (data) => {
   console.log(split);
 };
 
-const firstAndLast = (string) => {};
+const firstAndLast = (string) => {
+  let num;
+  let last;
+  let first;
+  const set = new Set(['1', '2', 3, 4, 5, 6, 7, 8, 9, 0]);
+  for (let i = 0; i < string.length; i++) {
+    if (set.has(string[i])) console.log(string[i]);
+  }
+};
+
