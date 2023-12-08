3ba31c92a87dab7d98297e6765e854ed0e6bee0a
Michael Newbold <newboldm3@gmail.com>
2023-12-08 14:07:59 -0800

    completed day2 part1

--- day2/day2.js
+++ day2/day2.js
@@ -13,3 +13,42 @@ const dataSplit = (data) => {
   const split = data.toString().split('\n');
   return split;
 };
+
+const maxOfGame = (game) => {
+  let splitGame = game.split(':');
+  let removed = splitGame.map((round) => {
+    return round
+      .replaceAll(' ', '')
+      .replaceAll('Game', '')
+      .replaceAll('reen', '')
+      .replaceAll('ed', '')
+      .replaceAll('lue', '');
+  });
+  removed = [removed[0], removed[1].split(';').join(',').split(',')];
+  let validGame = true;
+
+  const minNums = {
+    r: 0,
+    g: 0,
+    b: 0,
+  };
+
+  removed[1].forEach((pull) => {
+    const color = pull.slice(-1);
+    const num = pull.slice(0, -1);
+    if (num > minNums[color]) minNums[color] = num;
+  });
+  return minNums;
+};
+
+const multGames = (arr) => {
+  let sum = 0;
+  arr.forEach((game) => {
+    const minNums = maxOfGame(game);
+    const power = minNums[r] * minNums[g] * minNums[b];
+    sum += power;
+  });
+  return sum;
+};
+
+console.log(multGames(dataSplit(dataFetch())));
